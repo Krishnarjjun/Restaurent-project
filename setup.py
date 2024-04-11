23599c9b64a429c90429fae91df5831b7440e415
Krishna97 <kondrakrishna97@gmail.com>
2024-04-11 15:04:00 +0530

    model building

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,  find_packages
+
+setup(
+    name = "Restuarant Classification",
+    version= "0.0.1",
+    author= "Krishna Kondra",
+    author_email= "kondrakrishna97@gmail.com",
+    packages= find_packages()
+
+)
---
+++ src/RestuarantClassification/components/data_ingestion.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+import os
+
+df = pd.read_csv(r'C:\Users\Krishna.Arjun\Desktop\Krish\iNeuron\Practice work\Restaurent project\notebooks\data\restuarant.csv')
